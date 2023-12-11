51bec0d244dead92e54d9ca517968862be51931f
Joseph Camacho <spongkj@gmail.com>
2023-12-11 21:01:23 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,18 +28,41 @@
     });
   })();
 
+  import iconsSvg from '/src/images/icons.svg';
+
        // Function to toggle the SVG href
        function toggleSvgHref() {
          // Get the <use> element
          var useElement = document.getElementById('menuIcon');
 
          // Check the current href and toggle it
-         if (useElement.getAttributeNS('http://www.w3.org/1999/xlink', 'href') === '/src/images/icons.svg#gg_menu-left-icon') {
-             useElement.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/src/images/icons.svg#icon-x'); // Change to another icon
+         if (useElement.getAttributeNS('http://www.w3.org/1999/xlink', 'href') === '${iconSvg}#gg_menu-left-icon') {
+             useElement.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '${iconSvg}#icon-x'); // Change to another icon
          } else {
-             useElement.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/src/images/icons.svg#gg_menu-left-icon'); // Change back to the original icon
+             useElement.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '${iconSvg}#gg_menu-left-icon'); // Change back to the original icon
          }
      }
 
      // Add click event listener to the button
      document.getElementById('toggleButton').addEventListener('click', toggleSvgHref);
+
+// change-icon.js
+
+import image1Path from './src/images/icons.svg#gg_menu-left-icon';
+import image2Path from './src/images/icons.svg#icon-x';
+
+// Function to toggle the image source
+function toggleImage() {
+  // Get the image element
+  var image = document.getElementById('myImage');
+
+  // Check the current source and toggle it
+  if (image.src.endsWith(image1Path)) {
+      image.src = image2Path; // Change to the second image
+  } else {
+      image.src = image1Path; // Change back to the original image
+  }
+}
+
+// Add click event listener to the toggle button
+document.getElementById('toggleButton').addEventListener('click', toggleImage);
